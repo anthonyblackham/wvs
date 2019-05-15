2bcb3cca92400ced4dd7ee59d1f86084484dff74
anthonyblackham <anthonyblackham@gmail.com>
2019-05-15 10:01:00 -0700

    initial commits

---
+++ js/example.js
@@ -0,0 +1,86 @@
+var startDate = new Date();
+startDate.setUTCHours(0, 0, 0, 0);
+
+var map = L.map('map', {
+    zoom: 12,
+    fullscreenControl: true,
+    center: [39.3, 4]
+});
+
+// start of TimeDimension manual instantiation
+var timeDimension = new L.TimeDimension({
+        period: "PT1M",
+    });
+// helper to share the timeDimension object between all layers
+map.timeDimension = timeDimension; 
+// otherwise you have to set the 'timeDimension' option on all layers.
+
+var player        = new L.TimeDimension.Player({
+    transitionTime: 100, 
+    loop: false,
+    startOver:true
+}, timeDimension);
+
+var timeDimensionControlOptions = {
+    player:        player,
+    timeDimension: timeDimension,
+    position:      'bottomleft',
+    autoPlay:      true,
+    minSpeed:      1,
+    speedStep:     0.5,
+    maxSpeed:      15,
+    timeSliderDragUpdate: true
+};
+
+var timeDimensionControl = new L.Control.TimeDimension(timeDimensionControlOptions);
+map.addControl(timeDimensionControl);
+
+var icon = L.icon({
+    iconUrl: 'img/locomotive.png',
+    iconSize: [22, 22],
+    iconAnchor: [10, 25]
+});
+
+var customLayer = L.geoJson(null, {
+    pointToLayer: function (feature, latLng) {
+        if (feature.properties.hasOwnProperty('last')) {
+            return new L.Marker(latLng, {
+                icon: icon
+            });
+        }
+        return L.circleMarker(latLng);
+    }
+});
+
+var gpxLayer = omnivore.gpx('data/wvstrain1.gpx', null, customLayer).on('ready', function() {
+    map.fitBounds(gpxLayer.getBounds(), {
+        paddingBottomRight: [40, 40]
+    });
+});
+
+var gpxTimeLayer = L.timeDimension.layer.geoJson(gpxLayer, {
+    updateTimeDimension: true,
+    addlastPoint: true,
+    waitForReady: true
+});
+
+var pioneerLayer = L.tileLayer('https://a.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=db5ae1f5778a448ca662554581f283c5', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);;
+
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var baseMaps = {
+    "Pioneer layer": pioneerLayer,
+    "OSM Layer": osmLayer,
+};
+
+var overlayMaps = {
+    "Train 1": gpxTimeLayer,
+};
+//var baseLayers = getCommonBaseLayers(map); // see baselayers.js
+//osmLayer.addTo(map);
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+gpxTimeLayer.addTo(map);
